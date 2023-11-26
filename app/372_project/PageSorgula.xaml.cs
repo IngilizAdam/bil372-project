2bdb151ae7dea453e5acd4ef1dde487b99b443c6
IngilizAdam <ht@hasantuna.com>
2023-11-26 23:20:31 +0300

    added functionality for datagrid view

--- app/372_project/PageSorgula.xaml.cs
+++ app/372_project/PageSorgula.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using MySqlConnector;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
