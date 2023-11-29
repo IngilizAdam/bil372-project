e4ea8988e6b9570c1de16d848d49e01655aa73ed
IngilizAdam <ht@hasantuna.com>
2023-11-29 22:58:35 +0300

    updated triggers

--- mysql/SqlCode.sql
+++ mysql/SqlCode.sql
@@ -131,7 +131,7 @@ CREATE TABLE STUD_AVAIL_HOUR (
     CHECK (avail_day >= 0 AND avail_day <= 6),
     
     PRIMARY KEY (stud_id, avail_hour, avail_day),
-    FOREIGN KEY (stud_id) REFERENCES ACTIVE(stud_id)
+    FOREIGN KEY (stud_id) REFERENCES ACTIVE(stud_id) ON DELETE CASCADE
 );
 
 CREATE TABLE INST_AVAIL_HOUR (
@@ -143,7 +143,7 @@ CREATE TABLE INST_AVAIL_HOUR (
     CHECK (avail_day >= 0 AND avail_day <= 6),
     
     PRIMARY KEY (empl_id, avail_hour, avail_day),
-    FOREIGN KEY (empl_id) REFERENCES INSTRUCTOR(empl_id)
+    FOREIGN KEY (empl_id) REFERENCES INSTRUCTOR(empl_id) ON DELETE CASCADE
 );
 
 CREATE TABLE COUR_HOUR (
@@ -155,7 +155,7 @@ CREATE TABLE COUR_HOUR (
     CHECK (cour_day >= 0 AND cour_day <= 6),
     
     PRIMARY KEY (cour_id, cour_hour, cour_day),
-    FOREIGN KEY (cour_id) REFERENCES COURSE(cour_id)
+    FOREIGN KEY (cour_id) REFERENCES COURSE(cour_id) ON DELETE CASCADE
 );
 
 CREATE TABLE RELATIVE (
@@ -164,8 +164,8 @@ CREATE TABLE RELATIVE (
     rela_type VARCHAR(16),
     
     PRIMARY KEY (stud_id, pare_id),
-    FOREIGN KEY (stud_id) REFERENCES STUDENT(stud_id),
-    FOREIGN KEY (pare_id) REFERENCES PARENT(pare_id)
+    FOREIGN KEY (stud_id) REFERENCES STUDENT(stud_id) ON DELETE CASCADE,
+    FOREIGN KEY (pare_id) REFERENCES PARENT(pare_id) ON DELETE CASCADE
 );
 
 CREATE TABLE GIVEN_COURSE (
@@ -173,8 +173,8 @@ CREATE TABLE GIVEN_COURSE (
     cour_id CHAR(6),
     
     PRIMARY KEY(cour_id),
-    FOREIGN KEY(inst_id) REFERENCES INSTRUCTOR(empl_id),
-    FOREIGN KEY(cour_id) REFERENCES COURSE(cour_id)
+    FOREIGN KEY(inst_id) REFERENCES INSTRUCTOR(empl_id) ON DELETE CASCADE,
+    FOREIGN KEY(cour_id) REFERENCES COURSE(cour_id) ON DELETE CASCADE
 );
 
 CREATE TABLE REQUESTED_COURSE (
@@ -182,8 +182,8 @@ CREATE TABLE REQUESTED_COURSE (
     cour_id CHAR(6),
     
     PRIMARY KEY(stud_id, cour_id),
-    FOREIGN KEY(stud_id) REFERENCES ACTIVE(stud_id),
-    FOREIGN KEY(cour_id) REFERENCES COURSE(cour_id)
+    FOREIGN KEY(stud_id) REFERENCES ACTIVE(stud_id) ON DELETE CASCADE,
+    FOREIGN KEY(cour_id) REFERENCES COURSE(cour_id) ON DELETE CASCADE
 );
 
 CREATE TABLE TAKEN_COURSE (
@@ -191,8 +191,8 @@ CREATE TABLE TAKEN_COURSE (
     cour_id CHAR(6),
     
     PRIMARY KEY(stud_id, cour_id),
-    FOREIGN KEY(stud_id) REFERENCES ACTIVE(stud_id),
-    FOREIGN KEY(cour_id) REFERENCES COURSE(cour_id)
+    FOREIGN KEY(stud_id) REFERENCES ACTIVE(stud_id) ON DELETE CASCADE,
+    FOREIGN KEY(cour_id) REFERENCES COURSE(cour_id) ON DELETE CASCADE
 );
 
 CREATE TABLE NEEDED_STOCK (
@@ -201,89 +201,27 @@ CREATE TABLE NEEDED_STOCK (
     need_amount INT NOT NULL,
 
     PRIMARY KEY (cour_id, stck_id),        
-    FOREIGN KEY (cour_id) REFERENCES COURSE(cour_id),
-    FOREIGN KEY (stck_id) REFERENCES STOCK(stck_id)
+    FOREIGN KEY (cour_id) REFERENCES COURSE(cour_id) ON DELETE CASCADE,
+    FOREIGN KEY (stck_id) REFERENCES STOCK(stck_id) ON DELETE CASCADE
 );
 
--- EMPLOYEE  ------------------------------------------------
-
-
-
--- ADMIN_STAFF ------------------------------------------------
-
-create trigger DELETE_ADMIN_STAFF_EMPLOYEE after DELETE on ADMIN_STAFF for each row -- DELETE EMPLOYEE instance of a administrative_staff upon deletion
-		DELETE from EMPLOYEE e where e.empl_id=old.empl_id;
-	
--- INSTRUCTOR ------------------------------------------------
-	
-#create trigger UPDATE_COURSE_INST before DELETE on INSTRUCTOR for each row -- UPDATE a courses instructor to null when its instructor is deleted
-#		UPDATE COURSE c set c.cour_instructor=null where c.cour_instructor=old.empl_id;
-
-create trigger DELETE_INSTRUCTOR_EMPLOYEE after DELETE on INSTRUCTOR for each row -- DELETE EMPLOYEE instance of a teacher upon deletion
-	DELETE from EMPLOYEE e where e.empl_id=old.empl_id;
-
-create trigger DELETE_INST_AVAIL_HOUR before DELETE on INSTRUCTOR for each row -- DELETE a teachers available hours when he is deleted
-	DELETE from INST_AVAIL_HOUR iah where iah.empl_id=old.empl_id;
-
-
-
--- STAFF ------------------------------------------------
-	
-create trigger DELETE_STAFF_EMPLOYEE after DELETE on STAFF for each row -- DELETE EMPLOYEE instance of a staff upon deletion
-	DELETE from EMPLOYEE e where e.empl_id=old.empl_id;
 
 -- STUDENT ------------------------------------------------
 
+create trigger DELETE_GRADUATE_STUDENT before DELETE on STUDENT for each row -- DELETE a graduate students student when he is deleted
+	DELETE from GRADUATE g where g.stud_id=old.stud_id;
+
+create trigger DELETE_ACTIVE_STUDENT before DELETE on STUDENT for each row -- DELETE a active students student when he is deleted
+	DELETE from ACTIVE a where a.stud_id=old.stud_id;
 
 
+-- EMPLOYEE ------------------------------------------------
 
--- ACTIVE ------------------------------------------------
+create trigger DELETE_INSTRUCTOR_EMPLOYEE before DELETE on EMPLOYEE for each row
+	DELETE from INSTRUCTOR i where i.empl_id=old.empl_id;
 
-create trigger DELETE_STUD_AVAIL_HOUR before DELETE on ACTIVE for each row -- DELETE a active students available hours when he is deleted
-	DELETE from STUDENT_AVAIL_HOUR sah where sah.stud_id=old.stud_id;
+create trigger DELETE_STAFF_EMPLOYEE before DELETE on EMPLOYEE for each row
+	DELETE from STAFF s where s.empl_id=old.empl_id;
 
-create trigger DELETE_RELATIVE before DELETE on ACTIVE for each row -- DELETE a active students relatives when he is deleted
-	DELETE from RELATIVE r where r.stud_id=old.stud_id;
-
--- maybe add delete parent if single brother
- 
-create trigger DELETE_TAKEN_COURSE before DELETE on ACTIVE for each row -- DELETE a active students taken courses when he is deleted
-	DELETE from TAKEN_COURSE tc where tc.stud_id=old.stud_id;
-
-create trigger DELETE_REQUESTED_COURSE before DELETE on ACTIVE for each row -- DELETE a active students requested courses when he is deleted
-	DELETE from REQUESTED_COURSE rc where rc.stud_id=old.stud_id;
-
--- GRADUATE ------------------------------------------------
-
-create trigger DELETE_GRADUATE_STUDENT after DELETE on GRADUATE for each row -- DELETE a graduate students student when he is deleted
-	DELETE from STUDENT s where s.stud_id=old.stud_id;
-
--- PARENT ------------------------------------------------
-
-
-
--- EXPENSE ------------------------------------------------
-
-
-
--- STOCK ------------------------------------------------
-
-
-
--- COURSE ------------------------------------------------
-   
-create trigger DELETE_COURSE_REQUESTED_COURSE before DELETE on COURSE for each row -- DELETE requests of a course when it is deleted
-	DELETE from REQUESTED_COURSE rc where rc.cour_id=old.cour_id;
-
-create trigger DELETE_COURSE_TAKEN_COURSE before DELETE on COURSE for each row -- DELETE taken courses of a course when it is deleted
-	DELETE from TAKEN_COURSE tc where tc.cour_id=old.cour_id;
-
-delimiter $$
-create trigger DELETE_COURSE_TRIGGER before DELETE on COURSE for each row begin -- DELETE allocated lectures of a course when it is deleted
-	DELETE from COUR_HOUR cl where cl.cour_id=old.cour_id;
-	DELETE from GIVEN_COURSE gc where gc.cour_id=old.cour_id;
-end $$
-delimiter ;
-
-create trigger DELETE_COURSE_NEEDED_STOCK before DELETE on COURSE for each row -- DELETE needed stocks of a course when it is deleted
-	DELETE from NEEDED_STOCK ns where ns.cour_id=old.cour_id;
+create trigger DELETE_ADMIN_STAFF_EMPLOYEE before DELETE on EMPLOYEE for each row
+	DELETE from ADMIN_STAFF a where a.empl_id=old.empl_id;
